3fc7fa405b5178c35355c134d141eeb095b6343e
unknown <sanaeacharkaoui18@gmail.com>
2026-03-30 02:39:13 +0200

    fix: application du CSS et finalisation de la structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HeatMapPage from './pages/HeatMap/HeatMapPage'
 import AuthPage from './pages/Auth/AuthPage'
-import ClassementPage from './components/ClassementPage'
+import ClassementPage from './pages/classement/ClassementPage'
 import './App.css'
 
 function App() {

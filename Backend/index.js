c0f17122f2bd189e471d68ffd4e1b642f882282b
Abdellahi Mohamed Lemine <abdellahi.mohamed-lemine2@etu.univ-lorraine.fr>
2026-04-27 16:12:20 +0200

    fix(network): move to port 5001 to resolve collision and enable real data access

--- Backend/index.js
+++ Backend/index.js
@@ -39,7 +39,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('error serveur');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Serveur sur port ${PORT}`);
 });

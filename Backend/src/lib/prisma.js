7f64d0158e4a0b05d2ae86ed0be6900de07935f7
unknown <sanaeacharkaoui18@gmail.com>
2026-05-04 11:07:47 +0200

    sauvegarde : correction prisma et structure graphiques

--- Backend/src/lib/prisma.js
+++ Backend/src/lib/prisma.js
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
-import 'dotenv/config';
+import 'dotenv/config'
+
 const prismaClientSingleton = () => {
+  // On revient à un constructeur vide, le lien se fera via le npx prisma generate
   return new PrismaClient()
 }
 
